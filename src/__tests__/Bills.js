61d25fa2527a7bc6d8d0ecd56aa0b6a72cc50440
Alain Daly <alain.daly.93600@gmail.com>
2025-07-03 10:20:31 +0200

    add E2E test scenarios for employee

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -11,8 +11,12 @@ import router from "../app/Router.js"
 import Bills from "../containers/Bills.js"
 import mockStore from "../__mocks__/store.js"
 
+
+// Étant donné que je suis connecté en tant qu'employé
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+
+    // Test Unitaire : l’icône bills doit être surlignée
     test("Then bill icon in vertical layout should be highlighted", async () => {
 
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
@@ -32,6 +36,8 @@ describe("Given I am connected as an employee", () => {
       expect(windowIcon.classList.contains("active-icon")).toBe(true)
 
     })
+
+    // Test Unitaire : les factures doivent être triées du + récent au + ancien
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -41,7 +47,7 @@ describe("Given I am connected as an employee", () => {
     })
   })
   
-  // Test sur ce qui se passe quand on clique sur le bouton "Nouvelle note de frais"
+  // Test Unitaire : sur ce qui se passe quand on clique sur le bouton "Nouvelle note de frais"
   describe("When I click on 'Nouvelle note de frais'", () => {
     test("Then it should navigate to NewBill page", () => {
       // Afficher l'interface Bills
@@ -68,7 +74,7 @@ describe("Given I am connected as an employee", () => {
     })
   })
   
-  // Test si la page récupère bien les factures
+  // Test Unitaire : si la page récupère bien les factures
   describe("When I navigate to Bills", () => {
     test("Then it should fetch bills from mock API", async () => {
       // On va observe la fonction pour l'appel à mockStore.bills()
@@ -81,5 +87,42 @@ describe("Given I am connected as an employee", () => {
       expect(appelFactures).toHaveBeenCalled()
       expect(bills.length).toBeGreaterThan(0)
     })
-  }) 
+  })
+  
+  // Test Unitaire : Clic sur une icône œil donc modale s’ouvre
+  describe("When I click on the eye icon of a bill", () => {
+    test("Then a modal should open with the bill proof", () => {
+      // Injecte le HTML avec les données mockées
+      document.body.innerHTML = BillsUI({ data: bills, loading: false, error: null })
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      };
+
+      const billsContainer = new Bills({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage
+      });
+
+      // On évite une erreur Bootstrap en simulant $.fn.modal()
+      $.fn.modal = jest.fn()
+
+      // On récupère la première icône "œil"
+      const iconEye = screen.getAllByTestId("icon-eye")[0]
+
+      // On simule le clic
+      const handleClickIconEye = jest.fn(() => billsContainer.handleClickIconEye(iconEye))
+      iconEye.addEventListener("click", handleClickIconEye)
+      fireEvent.click(iconEye)
+
+      // On vérifie que la fonction handleClickIconEye a été appelée
+      expect(handleClickIconEye).toHaveBeenCalled()
+
+      // On vérifie que la modale est bien présente dans le DOM
+      const modal = screen.getByTestId("modaleFile")
+      expect(modal).toBeTruthy()
+    })
+  })
 })
